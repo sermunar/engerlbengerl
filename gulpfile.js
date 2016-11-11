7044618e5238cb4c9a4e226479d14173b587552d
Serkan Munar <munar@gmx.at>
2016-11-11 15:02:16 +0100

    Add base views and add html gulp tasks

--- gulpfile.js
+++ gulpfile.js
@@ -2,9 +2,14 @@ var gulp = require('gulp');
 var ts = require('gulp-typescript');
 var tsProject = ts.createProject('tsconfig.json');
 var sourcemaps = require('gulp-sourcemaps');
+var concat = require('gulp-concat');
 
 var config = {
-    appTs: 'app/**/*.ts'
+    appTs: 'app/**/*.ts',
+    headHtml: 'views/head.html',
+    templatesHtml: 'views/templates/**/*.html',
+    bodyHtml: 'views/body.html',
+    viewsHtml: 'views/**/*.html'
 };
 
 gulp.task('app-ts', function () {
@@ -17,15 +22,27 @@ gulp.task('app-ts', function () {
         .pipe(gulp.dest('./'));
 });
 
+gulp.task('index-html', function() {
+    return gulp.src([config.headHtml, config.templatesHtml, config.bodyHtml])
+        .pipe(concat('index.html'))
+        .pipe(gulp.dest('./'));
+});
 
 
 /*
     watch
 */
-gulp.task('watch-all', ['watch-app-ts']);
+gulp.task('watch-all', ['watch-app-ts', 'watch-html']);
 
 gulp.task('watch-app-ts',  function () {
     var watcher = gulp.watch(config.appTs, ['app-ts']);
+    watcher.on('change', function(event) {
+        console.log('File ' + event.path + ' was ' + event.type);
+    });
+});
+
+gulp.task('watch-html',  function () {
+    var watcher = gulp.watch(config.viewsHtml, ['index-html']);
     watcher.on('change', function(event) {
         console.log('File ' + event.path + ' was ' + event.type);
     });
